69812e488578c2061fd221198a3ecc6523744a23
sarangkharche <sarang@users.noreply.github.com>
2026-03-05 14:01:37 +0000

    Update Bolo to v1.0.0 with SHA256

--- Casks/bolo.rb
+++ Casks/bolo.rb
@@ -1,6 +1,6 @@
 cask "bolo" do
   version "1.0.0"
-  sha256 "CHECKSUM_HERE"
+  sha256 "602722551a45cab018933702b20e6bb3b4091d7f6dfb40990042627fc5b25aa1"
 
   url "https://rsbrckveklnximzfnczw.supabase.co/functions/v1/download?v=#{version}"
   name "Bolo"
